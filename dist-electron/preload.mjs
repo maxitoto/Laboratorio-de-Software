3271bd4fd0929f01d85205d61b013a820e83a804
Maxitoto <tommyush2000@hotmail.com>
2026-07-12 14:56:04 -0300

    actualización de dependencias

--- dist-electron/preload.mjs
+++
@@ -1,4 +0,0 @@
-let electron = require("electron");
-//#region src/backend/preload.ts
-electron.contextBridge.exposeInMainWorld("electronIpc", { invoke: (channel, ...args) => electron.ipcRenderer.invoke(channel, ...args) });
-//#endregion
